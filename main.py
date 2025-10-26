4acad895609916a775776c0e98111877d859aa79
Syedailsa <ilsasyeda24@gmail.com>
2025-10-26 20:16:09 +0500

    origin

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ app = FastAPI(title="Tax Agent API")
 # Allow your Next.js origin (dev)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ai-tax-assistant-for-pak-startups-f.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
